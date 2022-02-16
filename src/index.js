887ef10a13e2d9a41f05e01ada5cc3fceea62441
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-16 11:31:02 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function(context) {
     name: "docusaurus-plugin-help-widgets",
 
     injectHtmlTags() {
-      console.log("hello");
+      console.log("Nayan Patel - Plugin help-widget adding...");
       return {
         headTags: [
           {
@@ -13,7 +13,7 @@ module.exports = function(context) {
               w['help-widget']=o;w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
               js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
               js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
-            }(window, document, 'script', 'hw', 'https://cdn.jsdelivr.net/npm/docusaurus-plugin-help-widget@1.3.0/src/script.js'));
+            }(window, document, 'script', 'hw', 'https://cdn.jsdelivr.net/npm/docusaurus-plugin-help-widget@1.5.0/src/script.js'));
             hw('init', { });
             hw('message', '');
             `
