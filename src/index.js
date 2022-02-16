fc3fc7e5730f5bda2f98cc1b4946e8d2875f7a8e
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-16 11:06:48 +0000

    add

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ module.exports = function(context) {
               w['help-widget']=o;w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
               js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
               js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
-            }(window, document, 'script', 'hw', 'https://cdn.jsdelivr.net/npm/dsn-scripts@1.0.0/life-help-widget.js'));
+            }(window, document, 'script', 'hw', '/script'));
             hw('init', { });
             hw('message', '');
             `
